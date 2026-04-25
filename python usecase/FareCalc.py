8f92bb4a0193c93e7a84d26583f88d415c4c9e78
sanjana7358 <132884917+sanjana7358@users.noreply.github.com>
2026-04-25 23:06:17 +0530

    Update FareCalc.py

--- python usecase/FareCalc.py
+++ python usecase/FareCalc.py
@@ -1,68 +1,49 @@
 def calculate_fare(km, vehicle_type, hour):
     rates = {
-        "Economy": 10,
-        "Premium": 18,
-        "SUV": 25
+        "economy": 10,
+        "premium": 18,
+        "suv": 25
     }
-
     try:
         rate = rates[vehicle_type]
         base_fare = km * rate
-
         surge = False
         if 17 <= hour <= 20:
             base_fare *= 1.5
             surge = True
-
         return base_fare, surge
-
     except KeyError:
         return None, None
-
-
 def format_time(hour):
     display_hour = hour % 12
     if display_hour == 0:
         display_hour = 12
-
     period = "AM" if hour < 12 else "PM"
     return f"{display_hour}:00 {period}"
-
-
 while True:
-    print("\n===== FARE CALCULATOR =====")
-
-    vehicle = input("Enter vehicle type (Economy/Premium/SUV or Exit): ")
-
-    if vehicle.lower() == "exit":
-        print("Exiting FareCalc...")
+    print("\n===== fare calculator =====")
+    vehicle = input("enter vehicle type (economy/premium/suv or exit): ")
+    if vehicle == "exit":
+        print("exiting farecalc...")
         break
-
     try:
-        km = float(input("Enter distance (in km): "))
-        hour = int(input("Enter Travel Time (0-23): "))
-
+        km = float(input("enter distance (in km): "))
+        hour = int(input("enter travel time (0-23): "))
         fare, surge = calculate_fare(km, vehicle, hour)
-
         if fare is None:
-            print("Service Not Available")
+            print("service not available")
             continue
-
         formatted_time = format_time(hour)
-
-        print("\n===== FARE RECEIPT =====")
-        print(f"Distance: {km} km")
-        print(f"Vehicle: {vehicle}")
-        print(f"Time: {formatted_time}")
-        print(f"Base Fare: {round(fare / (1.5 if surge else 1), 2)}")
-
+        print("\n===== fare receipt =====")
+        print(f"distance: {km} km")
+        print(f"vehicle: {vehicle}")
+        print(f"time: {formatted_time}")
+        print(f"base fare: {round(fare / (1.5 if surge else 1), 2)}")
         if surge:
-            print("Surge Applied: Yes (1.5x)")
+            print("surge applied: yes (1.5x)")
         else:
-            print("Surge Applied: No")
-
-        print(f"Final Fare: {round(fare, 2)}")
+            print("surge applied: no")
+        print(f"final fare: {round(fare, 2)}")
         print("========================\n")
-
     except ValueError:
-        print("Invalid input")
+        print("invalid input")
